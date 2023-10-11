6bda1250245afe66adecd717dace88bdc4335071
AlexNovikova <alexadubinina@mail.ru>
2023-10-11 15:06:40 +0700

    ex005 change

--- example005/Program.cs
+++ example005/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+if(username.ToLower() == "sasha"){
+    Console.WriteLine("Ура, это же Саша");
+
+}
+else{
+    Console.WriteLine("Привет " + username);
+}
