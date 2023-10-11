d16b80ae30012833d433ae62b7fc0f167560f55d
AlexNovikova <alexadubinina@mail.ru>
2023-10-11 12:59:06 +0700

    example003

--- example003/Program.cs
+++ example003/Program.cs
@@ -6,7 +6,11 @@ namespace example003
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter number one");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number two");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(n1 + " + " + n2 + " = " + n1+n2);
         }
     }
 }
