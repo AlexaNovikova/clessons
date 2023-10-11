3aeecf86eb5118f2d6fb9bf3dded46eac87db1ab
AlexNovikova <alexadubinina@mail.ru>
2023-10-11 13:05:53 +0700

    change example004

--- example004/Program.cs
+++ example004/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter number one");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number two");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            double res = (double) n1 /n2; 
+            Console.WriteLine(n1 + " / " + n2 + " = " +  res);
